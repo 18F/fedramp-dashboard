6c37a4246f6b1908b694821436cdb360a55f2853
Bryan Allred <bryan.allred@gmail.com>
2016-07-11 15:07:44 -0400

    [TG-134 #closed] Timeline in details view

--- src/fedramp.components/product.component.js
+++ src/fedramp.components/product.component.js
@@ -35,5 +35,89 @@
             
             $state.go('fedramp.app.home', {}, { reload: true });
         };
+
+        /**
+         * Is the products status considered 'Ready'
+         * @public
+         * @memberof Components.Product
+         *
+         * @returns
+         *  A value indicating if the product ready
+         */
+        self.isReady = function () {
+            return self.model.designation === 'Ready';
+        };
+
+        /**
+         * Is the products status considered 'In Process'
+         * @public
+         * @memberof Components.Product
+         *
+         * @returns
+         *  A value indicating if the product is in processing
+         */
+        self.isProcessing = function () {
+            return self.model.designation === 'In-Process';
+        };
+
+        /**
+         * Is the products status considered 'Compliant'
+         * @public
+         * @memberof Components.Product
+         *
+         * @returns
+         *  A value indicating if the product is compliant
+         */
+        self.isCompliant = function () {
+            let compliant = ['Compliant', 'In PMO Review'];
+            return compliant.includes(self.model.designation);
+        };
+
+        /**
+         * The percent complete for the product status
+         * @public
+         * @memberof Components.Product
+         *
+         * @returns
+         *  A percentage as an integer
+         */
+        self.percentComplete = function () {
+            let percent = 0;
+
+            if (self.isReady()) {
+                percent = 30;
+            } else if (self.isProcessing()) {
+                percent = 50;
+            } else if (self.isCompliant()) {
+                percent = 100;
+            }
+
+            return percent;
+        };
+
+        /**
+         * The status message providing information concerning the
+         * estimated or final compliancy date.
+         * @public
+         * @memberof Components.Product
+         *
+         * @returns
+         *  Status message
+         */
+        self.statusMessage = function () {
+            let message = '';
+            
+            if (self.isCompliant()) {
+                message = 'Compliant Since ' + (self.model.authorizationDate || '');
+            } else {
+                if (self.model.expectedCompliance) {
+                    message = 'Estimated Compliance Date ' + self.model.expectedCompliance;
+                } else {
+                    message = 'This provider has not given an Estimated Compliance Date';
+                }
+            }
+
+            return message;
+        };
     }
 })();
--- test/fedramp.services/helper.service.test.js
+++ test/fedramp.services/helper.service.test.js
@@ -14,6 +14,13 @@ describe('The helper service', function () {
         expect(service.today()).toBeDefined();
     });
 
+    it('can convert date to mm/dd/YYYY', function () {
+        expect(service.toDate('2013-08-23T04:00:00.000Z')).toBe('08/23/2013');
+        expect(service.toDate('2013-08-23')).toBe('08/22/2013');
+        expect(service.toDate('08/23/2013')).toBe('08/23/2013');
+        expect(service.toDate(null)).toBe('');
+    });
+
     it('can slugify text', function () {
         expect(service.slugify('Test With - Data')).toBe('test-with---data');
         expect(service.slugify('Test With')).toBe('test-with');
