aa9e1cfab3ca61f032b98b740adac09afcd22b96
Bryan Allred <bryan.allred@gmail.com>
2016-07-14 16:11:16 -0400

    [TG-157 #ready-for-test] Toggling filter layout for 3PAOs

--- src/fedramp/home/assessors.controller.js
+++ src/fedramp/home/assessors.controller.js
@@ -14,12 +14,30 @@
     function AssessorsController ($log, assessors) {
         var self = this;
 
+        self.sidebar = false;
         self.filteredData = [];
         self.assessors = assessors;
+        self.reuseRangeOptions = reuseRangeOptions;
+        self.reuseRangeFilter = reuseRangeFilter;
         self.onUpdate = onUpdate;
 
-        function onUpdate(items){
+        function onUpdate (items) {
             self.filteredData = items;
+        }
+
+        function reuseRangeOptions (assessors) {
+            return [
+                {value: {min: 0, max: 5}, label: '0 - 5', selected: false}, 
+                {value: {min: 6, max: 10}, label: '5 - 10', selected: false},
+                {value: {min: 11, max: 1000}, label: '> 10', selected: false}];
+        }
+
+        function reuseRangeFilter (assessor, index, arr, selectedOptions) {
+            return selectedOptions.find(function (option) {
+                if (assessor.reuses >= option.value.min && assessor.reuses <= option.value.max) {
+                    return option;
+                }
+            });
         }
     }
 })();
