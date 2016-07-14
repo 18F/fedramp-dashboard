cf25247cc4310a01129ef07924fa2d0dff5ce32b
Bryan Allred <bryan.allred@gmail.com>
2016-07-14 16:54:33 -0400

    [TG-158 #ready-for-test] Agency filter toggling applied

--- src/fedramp/home/agencies.controller.js
+++ src/fedramp/home/agencies.controller.js
@@ -13,12 +13,31 @@
      */
     function AgenciesController ($log, agencies) {
         var self = this;
+
+        self.sidebar = true;
         self.filteredData = [];
         self.agencies = agencies;
+        self.reuseRangeOptions = reuseRangeOptions;
+        self.reuseRangeFilter = reuseRangeFilter;
         self.onUpdate = onUpdate;
-        console.log(agencies);
-        function onUpdate(items){
+
+        function onUpdate (items) {
             self.filteredData = items;
+        }
+
+        function reuseRangeOptions (agencies) {
+            return [
+                {value: {min: 0, max: 5}, label: '0 - 5', selected: false}, 
+                {value: {min: 6, max: 10}, label: '5 - 10', selected: false},
+                {value: {min: 11, max: 1000}, label: '> 10', selected: false}];
+        }
+
+        function reuseRangeFilter (agency, index, arr, selectedOptions) {
+            return selectedOptions.find(function (option) {
+                if (agency.reuses >= option.value.min && agency.reuses <= option.value.max) {
+                    return option;
+                }
+            });
         }
     }
 })();
