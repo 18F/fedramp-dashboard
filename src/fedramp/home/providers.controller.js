2b9e1a0c7ef4b431aa506eacda688a03dfb01209
Bryan Allred <bryan.allred@gmail.com>
2016-07-14 16:43:51 -0400

    [TG-155 #ready-for-test] Filter toggling for CSPs

--- src/fedramp/home/providers.controller.js
+++ src/fedramp/home/providers.controller.js
@@ -7,8 +7,6 @@
 
     ProvidersController.$inject = ['$log', 'providers'];
 
-
-
     /**
      * @constructor
      * @memberof Controllers
@@ -16,30 +14,30 @@
     function ProvidersController ($log, providers) {
         var self = this;
 
+        self.sidebar = true;
+        self.filteredData = [];
         self.providers = providers;
         self.reuseRangeOptions = reuseRangeOptions;
         self.reuseRangeFilter = reuseRangeFilter;
         self.onUpdate = onUpdate;
 
-        function onUpdate(items){
+        function onUpdate (items) {
             self.filteredData = items;
         }
 
-        function reuseRangeOptions(providers){
+        function reuseRangeOptions (providers) {
             return [
                 {value: {min: 0, max:5}, label: '0 - 5', selected: false}, 
                 {value: {min: 6, max:10}, label: '5 - 10', selected:false},
                 {value: {min: 11, max:1000}, label: '> 10', selected:false}];
         }
 
-        function reuseRangeFilter (provider, index, arr, selectedOptions){
-            var found = selectedOptions.find(function(option){
-                if(provider.reuses >= option.value.min && provider.reuses <= option.value.max){
+        function reuseRangeFilter (provider, index, arr, selectedOptions) {
+            return selectedOptions.find(function (option) {
+                if (provider.reuses >= option.value.min && provider.reuses <= option.value.max) {
                     return option;
                 }
             });
-            return found;
         }
-
     }
 })();
