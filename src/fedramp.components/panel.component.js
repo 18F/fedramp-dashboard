98ab67fe8cf280100ac1d31df7d13dfc2037339f
John Flores <johnsflores@gmail.com>
2016-07-26 08:19:12 -0400

    Added panel component that allows transclusion to automatically handle collapse

---
+++ src/fedramp.components/panel.component.js
@@ -0,0 +1,36 @@
+(function () {
+    'use strict';
+    angular
+        .module('fedramp.components')
+        .component('panel', {
+            transclude: true,
+            templateUrl: 'src/templates/components/panel.html',
+            controller: Panel,
+            controllerAs: 'controller',
+            bindings: {
+                header: '@',
+                iconClass: '@',
+                expand: '<'
+            }
+        });
+
+    Panel.$inject = [];
+
+    /**
+     * @constructor
+     * @memberof Components
+     */
+    function Panel () {
+        var self = this;
+        self.header = self.header || '';
+        self.expand = angular.isDefined(self.expand) || true;
+
+        // funcs
+        self.$onInit = $onInit;
+
+
+        function $onInit(){
+
+        }
+    }
+})();
