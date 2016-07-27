475720d247c8b8a3eb0c6bc15621b3724c394aef
John Flores <johnsflores@gmail.com>
2016-07-27 12:01:21 -0400

    Products are now grouped by provider.

--- src/fedramp.components/product-list.component.js
+++ src/fedramp.components/product-list.component.js
@@ -26,36 +26,77 @@
 
         self.$onInit = $onInit;
         self.createList = createList;
-        self.slugify = slugify;
 
         function $onInit(){
             createList();
         }
 
-        function createList(){
-            var providers = fedrampData.providers();
 
+        function createList(){
+            // Generate group provider/product object
+            var products = groupProviders();
+
+            // Create an array containing the sorted provider names
+            var sortedProductKeys = Object.keys(products).sort(function(a,b){
+                if(a.toLowerCase() < b.toLowerCase()){
+                    return -1;
+                }
+                if(a.toLowerCase() > b.toLowerCase()){
+                    return 1;
+                }
+                return 0;
+            });
+
+            // Since we can't sort objects (maps) by keys, we take original sorted
+            // array of providers and map each value to replace the provder string with
+            // an object.
+            sortedProductKeys = sortedProductKeys.map(function(v){
+                return {
+                    name: v,
+                    products: products[v]
+                };
+            });
+
+            self.groupedProducts = sortedProductKeys;
+            
+        }
+
+        /**
+         * Creates an object containing providers to products.
+         *
+         * @example
+         *
+         * {
+         *      'Amazon': ['AWS', 'SomeOtherProduct']
+         * }
+         *
+         * @returns
+         * Object containing provider as key and product array as value
+         */
+        function groupProviders(){
+            var providers = fedrampData.providers();
+            var products = {};
             self.products.reduce(function(arr, product){
                 for(let x = 0; x < providers.length; x++){
                     let provider = providers[x];
                     for(let y = 0; y < provider.products.length; y++){
                         var curProduct = provider.products[y];
                         if(curProduct.name === product){
-                            arr.push({
+                            if(!products[provider.name]){
+                                products[provider.name] = [];
+                            }
+                            products[provider.name].push({
                                 name: product,
                                 provider: provider.name,
+                                slugified: helperService.slugify(provider.name)
                             });
                             break;
                         }
                     }
                 }
                 return arr;
-            }, self.productList);
-
-        }
-
-        function slugify(slugme){
-            return helperService.slugify(slugme);
+            }, products);
+            return products;
         }
     }
 })();
