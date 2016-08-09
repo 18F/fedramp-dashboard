c9c3b46af34ea7b5f0402b97df307ac61cd83e39
Bryan Allred <bryan.allred@gmail.com>
2016-08-09 14:04:20 -0400

    [#32] Broke apart dictionary testing for routes

--- test/fedramp/fedramp.routes.test.js
+++ test/fedramp/fedramp.routes.test.js
@@ -25,37 +25,87 @@ describe('Fedramp routing', function(){
         $httpBackend.verifyNoOutstandingRequest();
 	});
 
-	it('can access all routes', function () {
-        var routes = [
-            { url: '/sitemap.xml', state: 'fedramp.sitemap'},
-            { url: '/search/aws', state: 'fedramp.app.search'},
-            { url: '/products', state: 'fedramp.app.home.products'},
-            { url: '/agencies', state: 'fedramp.app.home.agencies'},
-            { url: '/assessors', state: 'fedramp.app.home.assessors'},
-            { url: '/product/acme_anvils', state: 'fedramp.app.product.information'},
-            { url: '/product/acme_avils/versus/acme_holes', state: 'fedramp.app.product.comparison'},
-            { url: '/agency/disney', state: 'fedramp.app.agency.information'},
-            { url: '/agency/disney/versus/universal', state: 'fedramp.app.agency.comparison'},
-            { url: '/assessor/dick_tracy', state: 'fedramp.app.assessor.information'},
-            { url: '/assessor/dick_tracy/versus/carmen_sandiego', state: 'fedramp.app.assessor.comparison'}
-        ];
+    it ('can access the sitemap', function () {
+        $location.path('/sitemap.xml');
+        $rootScope.$digest();
+        $httpBackend.flush();
+        expect($state.current.name).toEqual('fedramp.sitemap');
+    });
 
-        for (var i = 0; i < routes.length; i++) {
-            var route = routes[i];
-            $location.path(route.url);
-            $rootScope.$digest();
+    it ('can access the dictionary', function () {
+        $location.path('/dictionary');
+        $rootScope.$digest();
+        $httpBackend.flush();
+        expect($state.current.name).toEqual('fedramp.app.dictionary');
+    });
 
-            try {
-                $httpBackend.flush();
-            } catch (e) {
-                // We catch an exception and proceed on our merry way
-                // because for some reason not routes need to be
-                // flushed.
-                //
-                // https://youtu.be/YbYWhdLO43Q
-            }
+    it ('can access search', function () {
+        $location.path('/search/aws');
+        $rootScope.$digest();
+        $httpBackend.flush();
+        expect($state.current.name).toEqual('fedramp.app.search');
+    });
 
-            expect($state.current.name).toEqual(route.state);
-		}
-	});
+    it ('can access the listing of products', function () {
+        $location.path('/products');
+        $rootScope.$digest();
+        $httpBackend.flush();
+        expect($state.current.name).toEqual('fedramp.app.home.products');
+    });
+
+    it ('can access the listing of agencies', function () {
+        $location.path('/agencies');
+        $rootScope.$digest();
+        $httpBackend.flush();
+        expect($state.current.name).toEqual('fedramp.app.home.agencies');
+    });
+
+    it ('can access the listing of assessors', function () {
+        $location.path('/assessors');
+        $rootScope.$digest();
+        $httpBackend.flush();
+        expect($state.current.name).toEqual('fedramp.app.home.assessors');
+    });
+
+    it ('can access product information', function () {
+        $location.path('/product/acme_anvils');
+        $rootScope.$digest();
+        $httpBackend.flush();
+        expect($state.current.name).toEqual('fedramp.app.product.information');
+    });
+
+    it ('can access product comparison', function () {
+        $location.path('/product/acme_anvils/versus/acme_holes');
+        $rootScope.$digest();
+        $httpBackend.flush();
+        expect($state.current.name).toEqual('fedramp.app.product.comparison');
+    });
+
+    it ('can access agency information', function () {
+        $location.path('/agency/disney');
+        $rootScope.$digest();
+        $httpBackend.flush();
+        expect($state.current.name).toEqual('fedramp.app.agency.information');
+    });
+
+    it ('can access agency comparison', function () {
+        $location.path('/agency/disney/versus/universal');
+        $rootScope.$digest();
+        $httpBackend.flush();
+        expect($state.current.name).toEqual('fedramp.app.agency.comparison');
+    });
+
+    it ('can access assessor information', function () {
+        $location.path('/assessor/dick_tracy');
+        $rootScope.$digest();
+        $httpBackend.flush();
+        expect($state.current.name).toEqual('fedramp.app.assessor.information');
+    });
+
+    it ('can access assessor comparison', function () {
+        $location.path('/assessor/dick_tracy/versus/carmen_sandiego');
+        $rootScope.$digest();
+        $httpBackend.flush();
+        expect($state.current.name).toEqual('fedramp.app.assessor.comparison');
+    });
 });
