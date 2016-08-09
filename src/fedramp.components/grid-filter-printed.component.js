03c305ac1cdc8b1bccd797ad350203a88cb3befd
John Flores <johnsflores@gmail.com>
2016-08-09 08:45:17 -0400

    Removed empty grid filter print controller so it uses default controller.

--- src/fedramp.components/grid-filter-printed.component.js
+++ src/fedramp.components/grid-filter-printed.component.js
@@ -1,29 +1,16 @@
 (function () {
     'use strict';
-
+    /**
+     * Used to render a friendly text representation of the filters applied when printing
+     * Uses default controller supplied by angular components.
+     */
     angular
         .module('fedramp.components')
         .component('gridFilterPrint', {
-            controller: GridFilterPrint,
             controllerAs: 'controller',
             templateUrl: 'src/templates/components/grid-filter-print.html',
             require: {
                 gridController: '^grid'
             }
         });
-
-    GridFilterPrint.$inject = [];
-
-    /**
-     * Displays a human-friendly textual representation of the currently applied filters. This
-     * is used when rendering the grid in print mode.
-     *
-     * Requires to be nested within a [Grid]{@link Components.Grid} component.
-     *
-     * @constructor
-     * @memberof Components
-     */
-    function GridFilterPrint () {
-        var self = this;
-    }
 })();
