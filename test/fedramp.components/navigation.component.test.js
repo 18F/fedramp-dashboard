9df73dc10b1b16a60b02f69711f11d891a3669ec
Bryan Allred <bryan.allred@gmail.com>
2016-08-10 12:13:40 -0400

    Include navigation component tests

---
+++ test/fedramp.components/navigation.component.test.js
@@ -0,0 +1,87 @@
+describe('The Navigation component', function () {
+    'use strict';
+
+    var component;
+    var controller;
+    var githubUrl;
+	var $httpBackend;
+    var $rootScope;
+    var $state;
+    var $location;
+    var helperService;
+
+    beforeEach(function () {
+        module('fedramp', 'fedramp.components');
+        inject(function (_$componentController_, $injector, _$rootScope_) {
+            // Inject
+            $rootScope = _$rootScope_;
+            $httpBackend = $injector.get('$httpBackend');
+            $state = $injector.get('$state');
+            $location = $injector.get('$location');
+            helperService = $injector.get('helperService');
+            githubUrl = $injector.get('dataUrl');
+
+            // Set up
+            $location.search({status: 'In Process'});
+            
+            // Instantiate
+            controller = _$componentController_;
+            component = controller(
+                'navigation',
+                {
+                    $state: $state,
+                    $location: $location,
+                    helperService: helperService
+                },
+                {});
+        });
+    });
+
+    it('should initialize navigation', function () {
+        expect(component).toBeDefined();
+        expect(component.toggleMobile).toBe(false);
+    });
+
+    it('should be able to determine the path', function () {
+        expect(component).toBeDefined();
+
+        $httpBackend.whenGET(githubUrl).respond(TestData.DataJsonHttpResponse);
+        $location.path('/products');
+        $rootScope.$digest();
+        $httpBackend.flush();
+        $httpBackend.verifyNoOutstandingExpectation(); 
+        $httpBackend.verifyNoOutstandingRequest();
+
+        expect($state.current.name).toEqual('fedramp.app.home.products');
+        expect(component.includes('**.products')).toBe(true);
+        expect(component.includes('**.agencies')).toBe(false);
+    });
+
+    it('should be able to determine the path with filter', function () {
+        expect(component).toBeDefined();
+
+        $httpBackend.whenGET(githubUrl).respond(TestData.DataJsonHttpResponse);
+        $location.path('/products');
+        $rootScope.$digest();
+        $httpBackend.flush();
+        $httpBackend.verifyNoOutstandingExpectation(); 
+        $httpBackend.verifyNoOutstandingRequest();
+
+        expect($state.current.name).toEqual('fedramp.app.home.products');
+        expect(component.includes('**.products', 'In Process')).toBe(true);
+        expect(component.includes('**.products', 'Compliant')).toBe(false);
+    });
+
+    it('should redirect with filters applied', function () {
+        expect(component).toBeDefined();
+
+        $httpBackend.whenGET(githubUrl).respond(TestData.DataJsonHttpResponse);
+        component.filterProducts('In Process');
+        $rootScope.$digest();
+        $httpBackend.flush();
+        $httpBackend.verifyNoOutstandingExpectation(); 
+        $httpBackend.verifyNoOutstandingRequest();
+
+        expect($state.current.name).toEqual('fedramp.app.home.products');
+    });
+});
